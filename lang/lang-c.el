6464d4ed57156dbfd08e55e6524bbb6d06c2379c
andreoss <andreoss@sdf.org>
2014-03-29 18:28:38 +0100

    Remove irony

--- lang/lang-c.el
+++ lang/lang-c.el
@@ -1,30 +1,14 @@
 ;;; lang-c --- C & C++
 ;;; Commentary:
 ;;; Code:
-
-(safe-wrap
- (lambda () (require 'irony))
- (lambda () (use-package irony)))
-
-(safe-wrap
- (lambda ()  (require 'irony-eldoc))
- (lambda ()  (use-package irony-eldoc))
- )
-
-(add-hook 'c++-mode-hook    'irony-mode)
-(add-hook 'c-mode-hook      'irony-mode)
-(add-hook 'irony-mode-hook  'irony-cdb-autosetup-compile-options)
-(add-hook 'irony-mode-hook  'irony-eldoc)
-
-
-;;(use-package c-eldoc)
-;; (use-package company-c-headers)
-
-;; (require 'elide-head)
-
-;; (add-hook 'c-mode-hook (lambda () (font-lock-mode -1)))
-;; (add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
-;; (add-hook 'c-mode-common-hook 'elide-head)
-
+(require 'lsp)
+(add-hook 'c++-mode-hook    'lsp)
+(add-hook 'c-mode-hook      'lsp)
+(use-package c-eldoc)
+(use-package company-c-headers)
+(require 'elide-head)
+(add-hook 'c-mode-hook (lambda () (font-lock-mode -1)))
+(add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
+(add-hook 'c-mode-common-hook 'elide-head)
 (provide 'lang-c)
 ;;; lang-c.el ends here
