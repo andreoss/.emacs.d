9ff21c9251df00c65901c31af64e9d3014ad59ca
andreoss <andreoss@sdf.org>
2014-06-16 10:07:33 +0100

    Use dashboard

--- conf/conf-appearence.el
+++ conf/conf-appearence.el
@@ -1,21 +1,31 @@
 ;;; conf-appearance --- Look and feel
 ;;; Commentary:
 ;;; Code:
+(use-package dashboard
+  :ensure t
+  :config
+    (dashboard-setup-startup-hook)
+    (setq dashboard-items '((recents  . 20)
+                            (projects . 20)))
+    (setq dashboard-banner-logo-title ""))
 (use-package better-defaults)
 (scroll-bar-mode +1)
-(load-theme 'tango t)
 (defun ai:setup-frame (frame)
   "Setup a FRAME."
   (setq frame (or frame (selected-frame)))
-  (with-selected-frame frame
-    (set-background-color "#ffffea")
-    (set-foreground-color "#000000")
-    )
-  (set-frame-width frame 80)
-  (set-frame-height frame 40)
-  (fringe-mode '(14 . 7))
-  (custom-set-faces
-   '(fringe ((t (:background "#f7f7da"))))))
+  (if-any-window-system
+   (load-theme 'tango t)
+   (with-selected-frame frame
+     (set-background-color "#ffffea")
+     (set-foreground-color "#000000")
+     )
+   (set-frame-parameter frame 'internal-border-width 16)
+   (set-frame-width frame 80)
+   (set-frame-height frame 40)
+   (fringe-mode '(14 . 7))
+   (custom-set-faces
+    '(fringe ((t (:background "#f7f7da"))))))
+  )
 (add-hook 'after-init-hook
           (lambda ()
             (ai:setup-frame nil)) t)
@@ -32,6 +42,12 @@
   (feebleline-show-previous-buffer   nil)
   :hook
   (after-init . feebleline-mode))
+(set-display-table-slot standard-display-table 'truncation
+                        (make-glyph-code ?… 'default))
+(set-display-table-slot standard-display-table 'wrap
+                        (make-glyph-code ?↩ 'default))
+(custom-set-faces
+ '(header-line ((t (:background "#f7f7da")))))
 (use-package fringe-current-line
   :config
   (setq-default fcl-fringe-bitmap 'vertical-bar)
@@ -41,5 +57,6 @@
 (setq pop-up-windows t)
 ;; Indicate buffer boundaries
 (setq-default indicate-buffer-boundaries '((top . left) (t . right)))
+(set-scroll-bar-mode 'right)
 (provide 'conf-appearence)
 ;;; conf-appearence ends here
