91f6ca1d293e9074900f33b959401c27c31679a8
andreoss <andreoss@sdf.org>
2014-09-10 13:44:18 +0100

    Fix theme

--- conf/conf-appearence.el
+++ conf/conf-appearence.el
@@ -9,27 +9,32 @@
                             (projects . 20)))
     (setq dashboard-banner-logo-title ""))
 (use-package better-defaults)
+
 (scroll-bar-mode +1)
+
 (defun ai:setup-frame (frame)
   "Setup a FRAME."
   (setq frame (or frame (selected-frame)))
   (if-any-window-system
-   (load-theme 'tango t)
-   (with-selected-frame frame
-     (set-background-color "#ffffea")
-     (set-foreground-color "#000000")
-     )
-   (set-frame-parameter frame 'internal-border-width 16)
-   (set-frame-width frame 80)
-   (set-frame-height frame 40)
-   (fringe-mode '(14 . 7))
+   ;;(load-theme 'adwaita)
+   (when (display-graphic-p)
+     (set-frame-parameter frame 'internal-border-width 16)
+     (set-frame-width frame 80)
+     (set-frame-height frame 40)
+     (fringe-mode '(14 . 7)))
    (custom-set-faces
     '(fringe ((t (:background "#f7f7da"))))))
   )
+
 (add-hook 'after-init-hook
           (lambda ()
             (ai:setup-frame nil)) t)
+
+(add-to-list 'initial-frame-alist '(background-color . "#ffffea"))
+(add-to-list 'default-frame-alist '(background-color . "#ffffea"))
+(add-to-list 'default-frame-alist '(alpha . (85 . 50)))
 (add-to-list 'after-make-frame-functions #'ai:setup-frame)
+
 (minibuffer-electric-default-mode +1)
 (add-hook 'whitespace-mode-hook (lambda () (font-lock-mode +1)))
 (hook! prog-mode-hook (font-lock-mode -1))
