21deb2c89cd372d6770f3e34b3f08d5941774192
andreoss <andreoss@sdf.org>
2014-12-09 22:23:42 +0100

    Rename to README

--- init.el
+++ init.el
@@ -4,6 +4,6 @@
 ;;; Code:
 ;; Enable readable backtraces when error occurs
 (org-babel-load-file
- (expand-file-name (concat user-emacs-directory "setup.org")))
+ (expand-file-name (concat user-emacs-directory "README.org")))
 (provide 'init)
 ;;; init.el ends here
