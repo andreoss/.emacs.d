c846abcdefafa449bf0fc31c7b11be49268b2332
andreoss <andreoss@sdf.org>
2020-06-18 11:03:41 +0000

    Always start LSP for c/c++

--- lang/lang-c.el
+++ lang/lang-c.el
@@ -2,13 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 (require 'lsp)
-(add-hook 'c++-mode-hook    'lsp)
-(add-hook 'c-mode-hook      'lsp)
+(require 'elide-head)
 (use-package c-eldoc)
 (use-package company-c-headers)
-(require 'elide-head)
-(add-hook 'c-mode-hook (lambda () (font-lock-mode -1)))
-(add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
+(use-package ccls)
+(add-hook 'c-mode-hook        'c-turn-on-eldoc-mode)
 (add-hook 'c-mode-common-hook 'elide-head)
+(add-hook 'c++-mode-hook    'lsp)
+(add-hook 'c-mode-hook      'lsp)
 (provide 'lang-c)
 ;;; lang-c.el ends here
