77c8d9b2adf2932fb6147fd2b5525f921a07a8fe
andreoss <andreoss@sdf.org>
2020-12-05 23:41:04 +0000

    Fix `evil-collection-init` failure

--- conf/conf-vc.el
+++ conf/conf-vc.el
@@ -1,28 +1,32 @@
 ;;; conf-vc --- Git Configuration
 ;;; Commentary:
 ;;; Code:
-(use-package magit)
-(use-package magit-gitflow
-  :after magit)
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . magit-status))
+(use-package magit-gitflow :after magit)
 (use-package evil-magit
-  :after magit
-  :init (evil-magit-init))
-(global-set-key (kbd "C-x g") 'magit-status)
+  :after (magit evil)
+  :config (evil-magit-init))
+;;(use-package evil-collection
+;;  :after (magit evil evil-magit)
+;;  :config (evil-collection-init))
 (use-package git-gutter
-  :diminish
-  :init
+  :config
   (global-git-gutter-mode +1))
 (use-package git-commit
   :hook ((git-commit-mode . flyspell-mode)
          (git-commit-mode . git-commit-save-message)
-         (git-commit-mode . turn-on-auto-fill)))
-(custom-set-faces
- '(git-gutter:added ((t (:inherit default :foreground "medium sea green" :weight bold))))
- '(git-gutter:deleted ((t (:inherit default :foreground "sienna" :weight bold))))
- '(git-gutter:modified ((t (:inherit default :foreground "dark orchid" :weight bold))))
- '(git-gutter:unchanged ((t (:inherit default :background "LemonChiffon4"))))
- )
-(use-package gitconfig-mode
+         (git-commit-mode . turn-on-auto-fill))
+  :config
+  (custom-set-faces
+   '(git-gutter:added ((t (:inherit default :foreground "medium sea green" :weight bold))))
+   '(git-gutter:deleted ((t (:inherit default :foreground "sienna" :weight bold))))
+   '(git-gutter:modified ((t (:inherit default :foreground "dark orchid" :weight bold))))
+   '(git-gutter:unchanged ((t (:inherit default :background "LemonChiffon4"))))
+   )
+  )
+(use-package gitconfig
   :mode (("/\\.gitconfig\\'"      . gitconfig-mode)
          ("/\\.git/config\\'"     . gitconfig-mode)
          ("/modules/.*/config\\'" . gitconfig-mode)
