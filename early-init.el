14a233d333d9a0403e50cba9e6ec77f5f1ee5376
andreoss <andreoss@sdf.org>
2023-01-28 21:05:12 -0500

    rmove bootstrap

--- early-init.el
+++ early-init.el
@@ -3,3 +3,22 @@
 ;;;
 ;;; Code:
 (setq package-enable-at-startup nil)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(straight-use-package 'use-package)
+(setq use-package-ensure-function 'straight-use-package)
+(setq straight-use-package-by-default t)
+(setq use-package-always-defer nil)
+(setq use-package-always-ensure t)
+(setq use-package-always-demand nil)
+(setq straight-vc-git-default-clone-depth 1)
