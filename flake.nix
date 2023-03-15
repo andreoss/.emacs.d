4f70065ca4c9e8680b9c8b89bb0e43c9e1213257
andreoss <andreoss@sdf.org>
2023-03-15 01:31:11 -0400

    add deps

--- flake.nix
+++ flake.nix
@@ -3,9 +3,18 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
+    jc-themes = {
+      url = "gitlab:andreoss/jc-themes/master";
+      flake = false;
+    };
+    elisp-autofmt = {
+      url = "git+https://codeberg.org/ideasman42/emacs-elisp-autofmt.git";
+      flake = false;
+    };
   };
   description = "emacs";
-  outputs = { self, nixpkgs, flake-utils, emacs-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, emacs-overlay, jc-themes
+    , elisp-autofmt, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         systems = lib.systems.flakeExposed;
@@ -23,11 +32,14 @@
           emacs = let pkgs = self.legacyPackages.${system};
           in pkgs.emacsWithPackagesFromUsePackage {
             alwaysEnsure = true;
-            config = ./init.el;
             defaultInitFile = true;
+            config = builtins.readFile (pkgs.substituteAll {
+              src = ./init.el;
+              jc = jc-themes;
+              autofmt = elisp-autofmt;
+            });
             package = pkgs.emacs;
           };
-          default = self.packages.${system}.emacs;
         };
       });
 }
