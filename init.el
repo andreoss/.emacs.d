d3618d9de85a52daf9d16ffbdf888f40a291532a
andreoss <andreoss@sdf.org>
2024-03-11 00:45:07 -0400

    treemacs

--- init.el
+++ init.el
@@ -73,9 +73,20 @@
     (treemacs-hide-gitignored-files-mode nil))
   :bind
   (:map global-map
+        ("M-1"       . treemacs)
+        ("C-x t t"   . treemacs)
         ("M-0"       . treemacs-select-window)
         ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
+        ("C-x t d"   . treemacs-select-directory)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :ensure t)
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
         ("C-x t d"   . treemacs-select-directory)
         ("C-x t B"   . treemacs-bookmark)
         ("C-x t C-t" . treemacs-find-file)
